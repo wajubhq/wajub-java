35429f5d51609b653e898b40e1d29fa881855bb5
Chapdel KAMGA <chapdel.kamga2@gmail.com>
2026-08-09 15:48:50 +0200

    Remove internal API-surface note from README, bump to 1.1.1

--- src/main/java/com/wajub/Version.java
+++ src/main/java/com/wajub/Version.java
@@ -3,7 +3,7 @@ package com.wajub;
 /** SDK release version sent in the User-Agent header. */
 public final class Version {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     private Version() {}
 }
